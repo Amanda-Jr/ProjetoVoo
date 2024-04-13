88353cbb1a9b2b6dd3fc0c5b5a19889896672341
William Pereira <almeidapereirast@gmail.com>
2024-04-13 14:00:08 -0300

    Update Pessoa.java

--- ProjetoVoo/src/vooProjeto/Pessoa.java
+++ ProjetoVoo/src/vooProjeto/Pessoa.java
@@ -1,5 +1,11 @@
 package vooProjeto;
 
 public class Pessoa {
-
+	protected String nome;
+	protected Data dataNasc;
+	
+	public Pessoa(String nome, Data dataNasc) {
+		this.nome = nome;
+		this.dataNasc = dataNasc;
+	}
 }
