8948690b6817266782cba6fc038d4531a1ad9f11
William Pereira <almeidapereirast@gmail.com>
2024-04-13 14:00:30 -0300

    Update Passageiro.java

--- ProjetoVoo/src/vooProjeto/Passageiro.java
+++ ProjetoVoo/src/vooProjeto/Passageiro.java
@@ -1,5 +1,47 @@
 package vooProjeto;
 
-public class Passageiro {
+public class Passageiro extends Pessoa {
+	private Voo passagem;
+	private int numMala;
+	private int pesoMala;
+	
+	public Passageiro(String nome, Data dataNasc, Voo passagem, int numMala, int pesoMala) {
+		super(nome, dataNasc);
+		this.passagem = passagem;
+		this.numMala = numMala;
+		this.pesoMala = pesoMala;
+	}
+	
+	public void embarcar() {
+		
+	}
+	
+	public void despacharMala() {
+		
+	}
+	
+	public Voo getPassagem() {
+		return passagem;
+	}
 
+	public void setPassagem(Voo passagem) {
+		this.passagem = passagem;
+	}
+
+	public int getNumMala() {
+		return numMala;
+	}
+
+	public void setNumMala(int numMala) {
+		this.numMala = numMala;
+	}
+
+	public int getPesoMala() {
+		return pesoMala;
+	}
+
+	public void setPesoMala(int pesoMala) {
+		this.pesoMala = pesoMala;
+	}
+	
 }
