6fd6327fab9d3cfbe1475224f370391b7b9987d2
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2024-04-14 22:14:24 -0300

    exibirAssentos

--- ProjetoVoo/src/vooProjeto/Aviao.java
+++ ProjetoVoo/src/vooProjeto/Aviao.java
@@ -11,12 +11,31 @@ public class Aviao {
 	//incompleto
 	public Aviao() {
 		this.assentos = new boolean[6][6];
+		inicializarAssentos();
+	}
+	public Aviao(Passageiro passageiro, Comissario comissario, Piloto piloto, Seguranca seguranca, Voo voo) {
+		this.assentos = new boolean[6][6];
+		inicializarAssentos();
+		this.passageiro = passageiro;
+		this.comissario = comissario;
+		this.piloto = piloto;
+		this.seguranca = seguranca;
+		this.voo = voo;
 		
+	}
+	
+	private void inicializarAssentos() {
+		for(int i=0; i<6; i++) {
+			for(int j=0; j<6; j++) {
+				this.assentos[i][j] = true;
+			}
+		}
 	}
 	
 	public boolean disponivel(String assento) {
 		String assentoPassageiro[] = assento.split("");
 		if(assentos[Integer.parseInt(assentoPassageiro[0])][Integer.parseInt(assentoPassageiro[1])] ) {
+			assentos[Integer.parseInt(assentoPassageiro[0])][Integer.parseInt(assentoPassageiro[1])] = false;
 			return true;
 		}
 		return false;
@@ -24,7 +43,23 @@ public class Aviao {
 	
 	public void decolar(){
 		
-	}	
+	}
+	
+	public void exibirAssentos() {
+		System.out.println("   1   2   3   4   5   6 ");
+		for(int i=0; i<6; i++) {
+			System.out.print(i+1);
+			for(int j=0; j<6; j++) {
+				if(assentos[i][j]) {
+					System.out.print("  " + i + "" + j);	
+				}
+				else {
+					System.out.print("  " + "x" + "" + "x");	
+				}
+			}
+			System.out.println(" ");
+		}
+	}
 	
 	public void checarPassageiro() {
 		
